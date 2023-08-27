33923e03d1b49a85603fa337d33f0a1cf730e8f0
Mike Meyer <mike.meyer86@web.de>
2023-08-27 16:51:54 +0200

    shopping list added

--- src/components/list/list.jsx
+++ src/components/list/list.jsx
@@ -1,45 +1,81 @@
+import { useState } from 'react';
+
 export default function List() {
+    const [newItem, setNewItem] = useState('');
+    const [listItems, setListItems] = useState([]);
+
+    function submitItem(event) {
+        event.preventDefault();
+        setListItems((currentListItems) => {
+            return [
+                ...currentListItems,
+                {
+                    id: crypto.randomUUID(),
+                    title: newItem,
+                    done: false,
+                },
+            ];
+        });
+        setNewItem('');
+    }
+
+    function toggleItem(itemId, done) {
+        setListItems((currentListItems) => {
+            return currentListItems.map((item) => {
+                if (item.id === itemId) {
+                    return { ...item, done };
+                }
+
+                return item;
+            });
+        });
+    }
+
+    function deleteItem(itemId) {
+        setListItems((currentListItems) => {
+            return currentListItems.filter((item) => item.id !== itemId);
+        });
+    }
+
     return (
         <>
-            <div className="container py-4 px-3 mx-auto">
-                <label className="px-1 mb-2" htmlFor="newListItemInput">
-                    Der Einkaufsliste hinzufügen:
-                </label>
-                <div className="input-group mb-3">
-                    <input
-                        type="text"
-                        className="form-control"
-                        placeholder="Schreib etwas..."
-                        aria-label="Shoppping list item"
-                        aria-describedby="button-addon2"
-                        id="newListItemInput"
-                    />
-                    <button className="btn btn-primary" type="button" id="addListItemButton">
-                        Hinzufügen
-                    </button>
+            <form onSubmit={submitItem}>
+                <div className="container py-4 px-3 mx-auto">
+                    <label className="px-1 mb-2" htmlFor="newListItemInput">
+                        Der Einkaufsliste hinzufügen:
+                    </label>
+                    <div className="input-group mb-3">
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Schreib etwas..."
+                            id="newListItemInput"
+                            value={newItem || ''}
+                            onChange={(event) => setNewItem(event.target.value)}
+                        />
+                        <button className="btn btn-primary" type="submit" id="addListItemButton">
+                            Hinzufügen
+                        </button>
+                    </div>
                 </div>
-            </div>
+            </form>
             <div className="container py-4 px-3 mx-auto">
                 <h3 className="px-1 mb-2">Einkaufsliste</h3>
                 <ul className="list-group">
-                    <li className="list-group-item">
-                        <label className="form-check-label stretched-link">
-                            <input className="form-check-input me-1" type="checkbox" />
-                            First Item
-                        </label>
-                    </li>
-                    <li className="list-group-item">
-                        <label className="form-check-label stretched-link">
-                            <input className="form-check-input me-1" type="checkbox" />
-                            Second Item
-                        </label>
-                    </li>
-                    <li className="list-group-item">
-                        <label className="form-check-label stretched-link">
-                            <input className="form-check-input me-1" type="checkbox" />
-                            Third Item
-                        </label>
-                    </li>
+                    {listItems.length === 0 && <span className="px-1">Es sind keine Einträge vorhanden.</span>}
+                    {listItems.map((item) => {
+                        return (
+                            <li className="list-group-item d-flex justify-content-between align-items-center" key={item.id}>
+                                <label className="form-check-label stretched-link d-flex gap-2">
+                                    <input className="form-check-input me-1" type="checkbox" defaultChecked={item.done} onChange={(e) => toggleItem(item.id, e.target.checked)} />
+                                    {item.title}
+                                </label>
+                                <button type="button" className="btn btn-sm btn-outline-danger z-1" onClick={() => deleteItem(item.id)}>
+                                    Entfernen
+                                </button>
+                            </li>
+                        );
+                    })}
                 </ul>
             </div>
         </>
