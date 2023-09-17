21412e48ff7a4adc40da8d6fa4f3ae0122efe360
Mike Meyer <mike.meyer86@web.de>
2023-09-17 14:28:59 +0200

    login form styled

--- src/components/main/login.jsx
+++ src/components/main/login.jsx
@@ -14,16 +14,30 @@ export default function Login({ signInUser }) {
     return (
         <>
             <div className="login-container">
+                <div className="login-header">
+                    <img src="/assets/img/logo.png" alt="Logo" />
+                    <span>familyApp</span>
+                </div>
                 <form className="login-form" onSubmit={handleSubmit}>
+                    <h5 className="mb-4">Bitte melde dich an:</h5>
                     <div className="form-group">
-                        <label htmlFor="email">Email</label>
-                        <input id="email" type="email" className="form-control" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                        <label htmlFor="email">E-Mail</label>
+                        <input
+                            id="email"
+                            type="email"
+                            className="form-control"
+                            placeholder="user@example.com"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                        />
                     </div>
                     <div className="form-group">
-                        <label htmlFor="password">Password</label>
+                        <label htmlFor="password">Passwort</label>
                         <input
                             id="password"
                             type="text"
+                            placeholder="Passwort"
                             className="form-control"
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
@@ -31,11 +45,16 @@ export default function Login({ signInUser }) {
                         />
                     </div>
                     <div className="form-group">
-                        <button type="submit" className="btn btn-primary">
+                        <button type="submit" className="btn btn-primary mt-2">
                             Anmelden
                         </button>
                     </div>
                 </form>
+                <div className="login-links">
+                    <span className="links pointer">Impressum</span>
+                    <span> | </span>
+                    <span className="links pointer">Datenschutz</span>
+                </div>
             </div>
         </>
     );
