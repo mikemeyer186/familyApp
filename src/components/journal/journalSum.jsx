55b7acff4e7d2cdb8fde773780e72c06cbfca991
Mike Meyer <mike.meyer86@web.de>
2023-11-16 22:19:31 +0100

    journal accordion header filled

--- src/components/journal/journalSum.jsx
+++ src/components/journal/journalSum.jsx
@@ -5,12 +5,58 @@ export default function JournalSum() {
     const { sumOfPayments } = useJournal();
     const income = sumOfPayments.filter((payment) => payment.aggregate === 'Einnahmen');
     const fixedCosts = sumOfPayments.filter((payment) => payment.aggregate === 'Fixkosten');
-    // const varCosts = sumOfPayments.filter((payment) => payment.aggregate !== 'Einnahmen' && payment.aggregate !== 'Fixkosten');
+    const varCosts = sumOfPayments.filter((payment) => payment.aggregate !== 'Einnahmen' && payment.aggregate !== 'Fixkosten');
+    const sumOfVarCosts = varCosts.reduce((acc, curr) => acc + curr.sum, 0);
+    const convertedIncome = income.length > 0 ? income[0].sum : 0.0;
+    const convertedFixedCosts = fixedCosts.length > 0 ? fixedCosts[0].sum : 0.0;
+    const convertedVarCosts = varCosts.length > 0 ? sumOfVarCosts : 0.0;
+
+    function incomeTemplate() {
+        return (
+            <div className="accordion-header-sum">
+                <span className="accordion-header-text">Einnahmen:</span>
+                <span className={convertedIncome > 0 ? 'income sum-text' : ''}>
+                    {`${convertedIncome.toLocaleString('de-DE', {
+                        style: 'currency',
+                        currency: 'EUR',
+                    })}`}
+                </span>
+            </div>
+        );
+    }
+
+    function fixedCostsTemplate() {
+        return (
+            <div className="accordion-header-sum">
+                <span className="accordion-header-text">Fixkosten:</span>
+                <span className={convertedFixedCosts < 0 ? 'spend sum-text' : ''}>
+                    {`${convertedFixedCosts.toLocaleString('de-DE', {
+                        style: 'currency',
+                        currency: 'EUR',
+                    })}`}
+                </span>
+            </div>
+        );
+    }
+
+    function varCostsTemplate() {
+        return (
+            <div className="accordion-header-sum">
+                <span className="accordion-header-text">Variable Kosten:</span>
+                <span className={convertedVarCosts < 0 ? 'spend sum-text' : ''}>
+                    {`${convertedVarCosts.toLocaleString('de-DE', {
+                        style: 'currency',
+                        currency: 'EUR',
+                    })}`}
+                </span>
+            </div>
+        );
+    }
 
     return (
         <>
             <Accordion multiple>
-                <AccordionTab header={`Einnahmen: ${income.length > 0 ? income[0].sum : 0}`}>
+                <AccordionTab headerTemplate={incomeTemplate}>
                     <p className="m-0">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
                         enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
@@ -18,7 +64,7 @@ export default function JournalSum() {
                         proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                     </p>
                 </AccordionTab>
-                <AccordionTab header={`Fixkosten: ${fixedCosts.length > 0 ? fixedCosts[0].sum : 0}`}>
+                <AccordionTab headerTemplate={fixedCostsTemplate}>
                     <p className="m-0">
                         Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque
                         ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
@@ -26,7 +72,7 @@ export default function JournalSum() {
                         Consectetur, adipisci velit, sed quia non numquam eius modi.
                     </p>
                 </AccordionTab>
-                <AccordionTab header={`Variable Kosten: `}>
+                <AccordionTab headerTemplate={varCostsTemplate}>
                     <p className="m-0">
                         At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos
                         dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt
