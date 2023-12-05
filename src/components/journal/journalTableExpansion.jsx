123581ab18d6425d4004ce1a9c2b651fe450fc96
Mike Meyer <mike.meyer86@web.de>
2023-12-05 19:43:07 +0100

    journal table styled

--- src/components/journal/journalTableExpansion.jsx
+++ src/components/journal/journalTableExpansion.jsx
@@ -8,22 +8,22 @@ export default function JournalTableExpansion({ data, setExpandedRows }) {
             <table className="table journal-detail-table">
                 <thead>
                     <tr>
-                        <th scope="col" className="fw-normal" style={{ width: '113px' }}>
+                        <th scope="col" className="fw-normal th-left" style={{ width: '113px' }}>
                             Von
                         </th>
                         <th scope="col" className="fw-normal" style={{ width: '315px' }}>
                             Beleginfo
                         </th>
-                        <th scope="col" className="fw-normal" style={{ width: '15%' }}>
+                        <th scope="col" className="fw-normal th-right" style={{ width: '15%' }}>
                             Bearbeiten
                         </th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td>{data.user}</td>
+                        <td className="td-left">{data.user}</td>
                         <td>{data.info}</td>
-                        <td className="journal-table-edit">
+                        <td className="journal-table-edit td-right">
                             <img
                                 src="/assets/icons/pencil-fill-black.svg"
                                 alt="Bearbeiten"
