512a0b2d7e8584fe1af6e5c049760bb682ed4750
Mike Meyer <mike.meyer86@web.de>
2023-12-06 20:49:40 +0100

    calendar added

--- src/components/calendar/calendarPage.jsx
+++ src/components/calendar/calendarPage.jsx
@@ -1,7 +1,35 @@
+import { Calendar, luxonLocalizer } from 'react-big-calendar';
+import { DateTime, Settings } from 'luxon';
+Settings.defaultZone = 'Europe/Berlin';
+
+const localizer = luxonLocalizer(DateTime, { firstDayOfWeek: 1 });
+const messages = {
+    week: 'Woche',
+    work_week: 'Arbeitswoche',
+    day: 'Tag',
+    month: 'Monat',
+    previous: 'zurück',
+    next: 'vor',
+    today: `heute`,
+    agenda: 'Übersicht',
+
+    showMore: (total) => `+${total} plus`,
+};
+
 export default function CalendarPage() {
     return (
         <>
-            <h1>Kalender</h1>
+            <div className="calendarPage-wrapper">
+                <Calendar
+                    culture={'de-DE'}
+                    messages={messages}
+                    // components={components}
+                    localizer={localizer}
+                    //   events={myEventsList}
+                    startAccessor="start"
+                    endAccessor="end"
+                />
+            </div>
         </>
     );
 }
