26f7263e1f0f66eb7f734884c428f5aaaeb02062
Mike Meyer <mike.meyer86@web.de>
2023-12-09 14:50:14 +0100

    daily motivation sentences added

--- src/components/dashboard/dashboardPage.jsx
+++ src/components/dashboard/dashboardPage.jsx
@@ -1,7 +1,18 @@
+import { useState } from 'react';
+import motivationSentences from '../../data/motivationSayings';
+
 export default function DashboardPage() {
+    const date = new Date();
+    const [yearDay] = useState(dayOfYear());
+
+    function dayOfYear() {
+        const day = Math.floor((date - new Date(date.getFullYear(), 0, 0)) / 1000 / 60 / 60 / 24);
+        return day;
+    }
     return (
         <>
             <h1>Dashboard</h1>
+            <p className="motivation-text">{motivationSentences[yearDay]}</p>
         </>
     );
 }
