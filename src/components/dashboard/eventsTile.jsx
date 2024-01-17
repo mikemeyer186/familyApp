bba22af0ebfebe039ab9108860c613f97a1cf7cd
Mike Meyer <mike.meyer86@web.de>
2024-01-17 18:42:05 +0100

    empty text in dashboard tiles added

--- src/components/dashboard/eventsTile.jsx
+++ src/components/dashboard/eventsTile.jsx
@@ -2,46 +2,50 @@ export default function EventsTile({ nextEvents }) {
     return (
         <div className="dashboard-tile">
             <h4 className="tile-title">Nächste Termine</h4>
-            {nextEvents.map((event) => {
-                return (
-                    <div className="tile-event" key={event.data.id}>
-                        <div className="event-left">
-                            <span>
-                                {event.start.toLocaleString('de-DE', {
-                                    day: '2-digit',
-                                    month: '2-digit',
-                                    year: 'numeric',
-                                })}
-                            </span>
+            {nextEvents.length > 0 ? (
+                nextEvents.map((event) => {
+                    return (
+                        <div className="tile-event" key={event.data.id}>
+                            <div className="event-left">
+                                <span>
+                                    {event.start.toLocaleString('de-DE', {
+                                        day: '2-digit',
+                                        month: '2-digit',
+                                        year: 'numeric',
+                                    })}
+                                </span>
 
-                            <div className="event-left-time">
-                                {!event.allDay ? (
-                                    <>
-                                        <span>
-                                            {event.start.toLocaleTimeString('de-DE', {
-                                                hour: '2-digit',
-                                                minute: '2-digit',
-                                            })}
-                                        </span>
-                                        <span> - </span>
-                                        <span>
-                                            {event.end.toLocaleTimeString('de-DE', {
-                                                hour: '2-digit',
-                                                minute: '2-digit',
-                                            })}
-                                        </span>
-                                    </>
-                                ) : (
-                                    <span>ganztägig</span>
-                                )}
+                                <div className="event-left-time">
+                                    {!event.allDay ? (
+                                        <>
+                                            <span>
+                                                {event.start.toLocaleTimeString('de-DE', {
+                                                    hour: '2-digit',
+                                                    minute: '2-digit',
+                                                })}
+                                            </span>
+                                            <span> - </span>
+                                            <span>
+                                                {event.end.toLocaleTimeString('de-DE', {
+                                                    hour: '2-digit',
+                                                    minute: '2-digit',
+                                                })}
+                                            </span>
+                                        </>
+                                    ) : (
+                                        <span>ganztägig</span>
+                                    )}
+                                </div>
+                            </div>
+                            <div className="event-right">
+                                <span>{event.title}</span>
                             </div>
                         </div>
-                        <div className="event-right">
-                            <span>{event.title}</span>
-                        </div>
-                    </div>
-                );
-            })}
+                    );
+                })
+            ) : (
+                <span className="tile-empty-text">In den nächsten 7 Tagen stehen keine Termine an</span>
+            )}
         </div>
     );
 }
--- src/components/dashboard/listsTile.jsx
+++ src/components/dashboard/listsTile.jsx
@@ -2,19 +2,23 @@ export default function ListsTile({ importantItems }) {
     return (
         <div className="dashboard-tile">
             <h4 className="tile-title">Wichtige Einkäufe</h4>
-            {importantItems.map((item) => {
-                return (
-                    <div className="tile-item" key={item.id}>
-                        <div className="item-left">
-                            <span className={`badge rounded-pill category ${item.category}`}>{item.category}</span>
+            {importantItems.length > 0 ? (
+                importantItems.map((item) => {
+                    return (
+                        <div className="tile-item" key={item.id}>
+                            <div className="item-left">
+                                <span className={`badge rounded-pill category ${item.category}`}>{item.category}</span>
+                            </div>
+                            <div className="item-right">
+                                <span className="item-title">{item.title}</span>
+                                <span className="item-user">{item.user}</span>
+                            </div>
                         </div>
-                        <div className="item-right">
-                            <span className="item-title">{item.title}</span>
-                            <span className="item-user">{item.user}</span>
-                        </div>
-                    </div>
-                );
-            })}
+                    );
+                })
+            ) : (
+                <span className="tile-empty-text">Es befinden sich gerade keine wichtigen Einträge auf deinen Listen</span>
+            )}
         </div>
     );
 }
