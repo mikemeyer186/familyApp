2c89db5e2161b247ce1d2ba459726a644adc00c5
Mike Meyer <mike.meyer86@web.de>
2024-01-25 22:09:47 +0100

    settings prepared

--- src/components/main/settings.jsx
+++ src/components/main/settings.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router';
 import { useLocalStorage } from '../../hooks/useLocalStorage';
+import listCategories from '../../data/listCategories';
 
 export default function Settings() {
     const [lastPage] = useLocalStorage('lastPage');
@@ -12,6 +13,7 @@ export default function Settings() {
     function handleSubmit(e) {
         e.preventDefault();
         //action
+        //save one single object to firestore (list, journal, calendar...)
         navigate(`/app/${lastPage}`);
     }
 
@@ -20,10 +22,18 @@ export default function Settings() {
             <div className="profile-content">
                 <div className="profil-header">
                     <h4 className="profil-title mb-2">Einstellungen</h4>
-                    <span>Hier kannst die Einstellungen für die App ändern</span>
+                    <span>Hier kannst die Einstellungen für die gesamte App ändern</span>
                 </div>
                 <div className="profile-body mt-5">
                     <form>
+                        <div className="mb-3">
+                            <h6 className="mb-3">Kategorien für Listen</h6>
+                            {listCategories.map((category, index) => {
+                                return (
+                                    <input key={index} type="text" id={`category${index}`} className="form-control mb-2" defaultValue={category} />
+                                );
+                            })}
+                        </div>
                         <div className="profile-footer mt-5">
                             <button type="button" className="btn btn-secondary" onClick={() => navigate(`/app/${lastPage}`)}>
                                 Abbrechen
