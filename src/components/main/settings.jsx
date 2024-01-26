12ac1313da04b1fc3c80ab06a553ad24bd779e8f
Mike Meyer <mike.meyer86@web.de>
2024-01-26 13:45:26 +0100

    addingand deleting of list categories added

--- src/components/main/settings.jsx
+++ src/components/main/settings.jsx
@@ -36,6 +36,29 @@ export default function Settings() {
     }
 
     /**
+     * deltes the category from list categories
+     * @param {number} index - index of category
+     */
+    function handleDeleteListCategory(index) {
+        let updatedCategories = listCategories.map((category) => ({ ...category }));
+        updatedCategories.splice(index, 1);
+        setListCategories(updatedCategories);
+        setNewAppSettings({ ...appSettings, list: updatedCategories });
+    }
+
+    /**
+     * adds a new category to list categories
+     * with empty text input and default color (can be changed by user)
+     */
+    function handleAddListCategory() {
+        let updatedCategories = listCategories.map((category) => ({ ...category }));
+        const newCategory = { category: '', color: '#3f55ab' };
+        updatedCategories.push(newCategory);
+        setListCategories(updatedCategories);
+        setNewAppSettings({ ...appSettings, list: updatedCategories });
+    }
+
+    /**
      * handles the abort of settings and resets the state to old value
      */
     function handleAbortSettings() {
@@ -63,6 +86,7 @@ export default function Settings() {
                                             className="form-control"
                                             value={listCategories[index].category}
                                             onChange={(e) => handleListCategoryChange(e.target.value, index, 'category')}
+                                            required
                                         />
                                         <input
                                             type="color"
@@ -70,15 +94,31 @@ export default function Settings() {
                                             className="form-control settings-list-color"
                                             value={listCategories[index].color}
                                             onChange={(e) => handleListCategoryChange(e.target.value, index, 'color')}
+                                            required
                                         ></input>
                                         {index > 0 ? (
-                                            <img src="/assets/icons/dash-circle.svg" alt="Delete" className="iconClickable delete-icon" />
+                                            <img
+                                                src="/assets/icons/dash-circle.svg"
+                                                alt="Löschen"
+                                                className="iconClickable delete-icon"
+                                                onClick={() => handleDeleteListCategory(index)}
+                                            />
                                         ) : (
                                             <div className="delete-icon-invisible"></div>
                                         )}
                                     </div>
                                 );
                             })}
+                            {listCategories.length < 10 && (
+                                <div className="add-list-category mt-3">
+                                    <img
+                                        src="/assets/icons/plus-circle.svg"
+                                        alt="Hinzufügen"
+                                        className="iconClickable add-icon"
+                                        onClick={handleAddListCategory}
+                                    />
+                                </div>
+                            )}
                         </div>
                         <div className="profile-footer mt-5">
                             <button type="button" className="btn btn-secondary" onClick={handleAbortSettings}>
