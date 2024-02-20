131dc6c292a662ea57412a9e43a7b0a457af64e4
Mike Meyer <mike.meyer86@web.de>
2024-02-20 22:23:09 +0100

    lazy spinner animation improved

--- src/components/global/lazySpinner.jsx
+++ src/components/global/lazySpinner.jsx
@@ -1,9 +1,9 @@
 export default function LazySpinner() {
     return (
         <div style={{ display: 'flex', gap: '8px', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
-            <div className="spinner-grow" role="status"></div>
-            <div className="spinner-grow" role="status"></div>
-            <div className="spinner-grow" role="status"></div>
+            <div className="spinner-grow ls-1" role="status"></div>
+            <div className="spinner-grow ls-2" role="status"></div>
+            <div className="spinner-grow ls-3" role="status"></div>
         </div>
     );
 }
