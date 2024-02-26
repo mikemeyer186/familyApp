82022ccbc6ec09214688e1a6fe0490531347517d
Mike Meyer <mike.meyer86@web.de>
2024-02-26 18:59:06 +0100

    includeAssets added to manifest

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react(),
         VitePWA({
             registerType: 'autoUpdate',
+            includeAssets: ['assets/fonts/*.ttf', 'assets/img/*.png', 'assets/img/*.webp', 'assets/icons/*.svg'],
             manifest: {
                 name: 'Family App',
                 short_name: 'familyApp',
