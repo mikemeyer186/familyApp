6d66a496432708dcb2a3bc9de650e0d28dfb1368
Mike Meyer <mike.meyer86@web.de>
2024-03-12 12:37:44 +0100

    calendar refactored

---
+++ src/components/calendar/calendarContent.jsx
@@ -0,0 +1,39 @@
+import { Calendar } from 'react-big-calendar';
+import { useCalendar } from '../../contexts/calendarContext';
+import CalendarPageToolbar from './calendarPageToolbar';
+import CalendarConfig from '../../config/rbc';
+
+export default function CalendarContent() {
+    const { events, onSelectEvent, onSelectTimeSlot, getDrilldownView } = useCalendar();
+    const { localizer, messages, formats, components, min, max } = CalendarConfig();
+
+    return (
+        <>
+            <div className="calendarToolbar">
+                <CalendarPageToolbar />
+            </div>
+            <Calendar
+                localizer={localizer}
+                culture={'de-DE'}
+                messages={messages}
+                defaultView="agenda"
+                views={['agenda', 'week', 'month']}
+                formats={formats}
+                components={components}
+                startAccessor="start"
+                endAccessor="end"
+                min={min}
+                max={max}
+                length={14}
+                events={events}
+                dayLayoutAlgorithm={'no-overlap'}
+                showAllEvents={false}
+                getDrilldownView={getDrilldownView}
+                longPressThreshold={100}
+                onSelectEvent={onSelectEvent}
+                onSelectSlot={onSelectTimeSlot}
+                selectable
+            />
+        </>
+    );
+}
--- src/components/calendar/calendarPage.jsx
+++ src/components/calendar/calendarPage.jsx
@@ -1,45 +1,9 @@
-import { Calendar } from 'react-big-calendar';
 import { useCalendar } from '../../contexts/calendarContext';
-import CalendarConfig from '../../config/rbc';
 import Spinner from '../global/spinner';
-import CalendarPageToolbar from './calendarPageToolbar';
+import CalendarContent from './calendarContent';
 
 export default function CalendarPage() {
-    const { isCalendarLoaded, events, onSelectEvent, onSelectTimeSlot, getDrilldownView } = useCalendar();
-    const { localizer, messages, formats, components, min, max } = CalendarConfig();
+    const { isCalendarLoaded } = useCalendar();
 
-    return (
-        <div className="calendarPage-wrapper">
-            <div className="calendarToolbar">
-                <CalendarPageToolbar />
-            </div>
-
-            {!isCalendarLoaded ? (
-                <Spinner>{'Kalender laden...'}</Spinner>
-            ) : (
-                <Calendar
-                    localizer={localizer}
-                    culture={'de-DE'}
-                    messages={messages}
-                    defaultView="agenda"
-                    views={['agenda', 'week', 'month']}
-                    formats={formats}
-                    components={components}
-                    startAccessor="start"
-                    endAccessor="end"
-                    min={min}
-                    max={max}
-                    length={14}
-                    events={events}
-                    dayLayoutAlgorithm={'no-overlap'}
-                    showAllEvents={false}
-                    getDrilldownView={getDrilldownView}
-                    longPressThreshold={100}
-                    onSelectEvent={onSelectEvent}
-                    onSelectSlot={onSelectTimeSlot}
-                    selectable
-                />
-            )}
-        </div>
-    );
+    return <div className="calendarPage-wrapper">{!isCalendarLoaded ? <Spinner>{'Kalender laden...'}</Spinner> : <CalendarContent />}</div>;
 }
